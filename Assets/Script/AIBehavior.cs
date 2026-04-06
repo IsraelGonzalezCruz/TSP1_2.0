cd39d6b80787c4e85b5016796002a72abe3bd458
IsraelGonzalezCruz <isr18el9tpcruz@gmail.com>
2026-04-06 12:51:54 -0600

    Laberinto

---
+++ Assets/Script/AIBehavior.cs
@@ -0,0 +1,141 @@
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.AI;
+
+public class AIBehavior : MonoBehaviour
+{
+    [SerializeField ]
+
+    public GameObject player;
+    [SerializeField]
+    public Transform entrance;
+    [SerializeField]
+    public Transform exit;
+
+    public float detectionRange = 2f;
+    public float exitRange = 2f;
+    public float minDistanceFromEntrance = 15f;
+
+    List<NavMeshAgent> agents = new ();
+
+    NavMeshTriangulation triangulation;
+    Vector3 entrancePos;
+
+    float detencionRangeSqr;
+    float exitRangeSqr;
+    float minDistanceSqr;
+
+    bool gameWon = false;
+
+    System.Random random = new();
+
+
+    void Start()
+    {
+        entrancePos = entrance.position;
+        triangulation = NavMesh.CalculateTriangulation();
+        detencionRangeSqr = detectionRange * detectionRange;
+        exitRangeSqr = Mathf.Pow(exitRange,2);
+        minDistanceSqr = Mathf.Pow(minDistanceFromEntrance, 2);
+
+        FindAllEnemies();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 playerPos = player.transform.position;
+
+        // Deteccion de enemigos en posición de jugador
+
+        bool playerCaught = false;
+
+        foreach (var agent in agents)
+        {
+            if (!agent.enabled) continue;
+            
+            if((agent.transform.position - playerPos).sqrMagnitude < detencionRangeSqr)
+                    {
+                playerCaught = true;
+                break;
+            }
+        }
+    }
+
+    //Método para llevar a player a la entrada
+    void TeleportPlayerToEntrance()
+    {
+        var cc = player.GetComponent<NavMeshAgent>();
+
+        if (cc != null)
+        {
+            cc.enabled = false;
+        }
+        player.transform.position = entrancePos;
+        if (cc != null)
+        {
+            cc.enabled = false;
+        }
+
+        Debug.Log("Player teleported to "+ entrancePos);   
+    }
+
+    //Método para posicionar a los enenmigos
+
+    void RelocateAllNPC()
+    {
+        if(triangulation.vertices.Length == 0) return;
+        foreach (var agent in agents)
+        {
+            agent.enabled = false;
+            //  agent.transform.position = GetRandomPositionOnNavMesh();
+            agent.enabled = true;
+        }
+    }
+
+    //Método para calcular posiciones válidas
+
+    Vector3 GetValidRandomPosition()
+    {
+        Vector3 pos;
+
+        do
+        {
+            int i = random.Next(0, triangulation.indices.Length / 3)*3;
+            Vector3 v1 = triangulation.vertices[triangulation.indices[i]];
+            Vector3 v2 = triangulation.vertices[triangulation.indices[i + 1]];
+            Vector3 v3 = triangulation.vertices[triangulation.indices[i + 2]];
+
+            float r1 = (float)random.NextDouble();
+            float r2 = (float)random.NextDouble();
+
+            if ((r1+r2)>1f)
+            {
+                r1= 1f - r1;
+                r2= 1f - r2;
+            }
+
+            pos = v1 + r1 * (v2 - v1) + r2 * (v3 - v1);
+
+        }
+        while ((pos - entrancePos).sqrMagnitude < minDistanceSqr);
+        return pos;
+
+    }
+
+    //Método para ubicar los objetos enemigos
+
+    void FindAllEnemies()
+    {
+        agents.Clear();
+        foreach (var agent in FindObjectsByType<NavMeshAgent>(FindObjectsSortMode.None))
+        {
+            if(agent.CompareTag("Enemy"))
+            {
+                agents.Add(agent);
+            }
+        }
+    }
+
+}
